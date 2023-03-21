e964621bc9db3a5d85f2aaeb68983eaa02955aca
kohei <koheimatsuno99@gmail.com>
2023-03-22 00:28:47 +0900

    環境構築

--- trump-game/src/App.tsx
+++ trump-game/src/App.tsx
@@ -1,19 +1,33 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import game from './phaser';
 
 function App() {
-  useEffect(() => {
-    game.scene.start('default');
-  }, []);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsButtonClicked(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <div id="game"/>
+      <header className="App-header" />
+      {!isButtonClicked && (
+        <div className=''>
+            <div id="lobby" style={{ backgroundColor: '#008000', padding: '10px' }} className="bg-warning ">
+              <h2 style={{ color: '#ffffff' }}>Game Lobby</h2>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <button style={{ padding: '10px', margin: '10px' }} onClick={handleClick}>Black Jack</button>
+              </div>
+            </div>
+        </div>
+      )}
+      <div id="game" />
     </div>
   );
 }
 
-
 export default App;
+
+
+
