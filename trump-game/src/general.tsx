32443fbb50ea8fd5e55501d29d54266b187163aa
kohei <koheimatsuno99@gmail.com>
2023-03-23 23:21:04 +0900

    created Table/Deck and modified Card

--- trump-game/src/general.tsx
+++ trump-game/src/general.tsx
@@ -1,7 +1,8 @@
 export class Card{
     private suit : string;
     private number : string;
-    private valueList : string[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    private static valueList : string[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    private static suitList : string[] = ["♠︎", "♦︎", "♥︎", "♣︎"];
 
     constructor(suit : string, number : string){
         this.suit = suit;
@@ -15,12 +16,40 @@ export class Card{
         return this.number;
     }
     public getValue() : number{
-        return this.valueList.indexOf(this.number) + 1;
+        return Card.valueList.indexOf(this.number) + 1;
+    }
+    public static getValueList() : string[]{
+        return Card.valueList;
+    }
+    public static getSuitList() : string[]{
+        return Card.suitList;
     }
 }
 
 export class Deck{
+    private deck : Card[] = [];
+    constructor(){
+        for(let suit in Card.getSuitList()){
+            for(let value in Card.getValueList()){
+                this.deck.push(new Card(suit, value));
+            }
+        }    
+    }
+    //デッキの一番後ろからカードを1枚引く
+    public draw() : Card{
+        let drawnCard : Card  = this.deck[this.deck.length - 1];
+        this.deck.pop();
+        return drawnCard;
+    }
 
+    public shuffle() : void{
+        for(let index = this.deck.length - 1; index > 0; index--){
+            let randomIndex = Math.floor(Math.random() * (index + 1));
+            let temp = this.deck[index];
+            this.deck[index] = this.deck[randomIndex];
+            this.deck[randomIndex] = temp;
+        }
+    }
 }
 
 export class Player{
@@ -33,6 +62,7 @@ export class Player{
         this.name = name;
         this.type = type;
         this.score = 0;
+        this.hand = [];
     }
 
     public getName() : string{
@@ -57,8 +87,10 @@ export class Player{
 
 export class Table{
     protected players : Player[];
+    protected deck : Deck;
+
     constructor(players : Player[]){
         this.players = players;
-
+        this.deck = new Deck();
     }
 }
