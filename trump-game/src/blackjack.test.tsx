99efd071df81ddbed941e02c733181816174d45f
propan <ldspdk5v@gmail.com>
2023-03-24 22:10:17 +0900

    #5 implement bet

--- trump-game/src/blackjack.test.tsx
+++ trump-game/src/blackjack.test.tsx
@@ -1,17 +1,17 @@
 import {BlackJackPlayer, BlackJackTable} from './blackjack';
 
 
-let player : BlackJackPlayer = new BlackJackPlayer("jack", "player", 30000);
+let player : BlackJackPlayer = new BlackJackPlayer("jack", "player");
 let table : BlackJackTable = new BlackJackTable(player);
 
-test("betTurnによってbetsが変更されている", () => {
-  table.betTurn();
+test("betPhase()によってbetsが変更されている", () => {
+  table.betPhase();
   const result = table.getBets();
 	expect(result).not.toBe([0,0,0]);
 });
 
 test("プレイヤーに掛け金がセットされる", () =>{
-  table.betTurn();
+  table.betPhase();
   const result = table.getPlayers()[1].getLatch();
   expect(result).toBe(100);
 })
--- trump-game/src/blackjack.tsx
+++ trump-game/src/blackjack.tsx
@@ -2,11 +2,13 @@ import {Card, Deck, Player, Table} from './general'
 
 export class BlackJackPlayer extends Player{
     private latch : number;
+    private chips : number;
 
-    constructor(name : string, type : string, latch : number){
+    constructor(name : string, type : string){
         super(name, type);
-        this.score = 40000;
-        this.latch = latch;
+        this.score = 0;
+        this.latch = 0;
+        this.chips = 40000;
     }
 
     public getLatch() : number{
@@ -15,47 +17,38 @@ export class BlackJackPlayer extends Player{
     public setLatch(value : number) : void{
         this.latch = value;
     }
+    public getChips() : number{
+        return this.chips;
+    }
+    public setChips(chip : number) : void{
+        this.chips = chip
+    }
+    public addChips(chip : number) : number{
+        this.chips += chip;
+        return this.chips;
+    }
 
-    public bet(cost: number) : number{
-        if(this.score >= cost){
-            this.setScore(this.getScore()-cost);
+    // 掛け金をかける。cost <= this.chipsならばthis.chipsが入力分減り、this.latchにセットされる。cost > this.chipsなら何も処理されない。
+    public bet(cost: number) : void{
+        if(this.chips >= cost){
+            this.setChips(this.getChips()-cost);
             this.setLatch(cost);
         }
-
-        return cost;
     }
 }
 
 export class BlackJackTable extends Table{
     private house : Player = new Player("House", "House");
     private roundNumber : number = 1;
-    private turnNumber : number = 0;
-    private phase : string = "betting";
-    private bets : number[] = [0, 0, 0];
+    private turnNumber : number = 0; // 1に変更の可能性あり
+    private phase : string = "betting"; // betting, dear, playerPhase, dealerPhaseなどに1roundの中で適宜変更される。不要なら削除もあり。
+    private bets : number[] = [0, 0, 0]; // 仮置き
     protected players : BlackJackPlayer[];
 
     constructor(player: BlackJackPlayer){
         super();
         // 仮置き
-        this.players = [new BlackJackPlayer("CPU1", "CPU", 40000), player, new BlackJackPlayer("CPU2", "CPU", 30000)];
-    }
-
-    public betTurn() : void{
-        for(let i : number = 0; i < this.players.length; ++i){
-            let current : BlackJackPlayer = this.players[i];
-            if(current.getScore() === 0) continue;
-
-            let bet : number = 0;
-            if(current.getType() === "CPU"){
-                bet = Math.random()*(1 - this.players[i].getScore()+1) + 1;
-            }
-            else {
-                bet = 100 // プレイヤーからの入力を受け取る
-                current.bet(bet);
-            }
-            this.bets[i] = bet;
-            current.bet(bet)
-        }
+        this.players = [new BlackJackPlayer("CPU1", "CPU"), player, new BlackJackPlayer("CPU2", "CPU")];
     }
 
     public getBets() : number[] {
@@ -64,5 +57,26 @@ export class BlackJackTable extends Table{
 
     public getPlayers() : BlackJackPlayer[] {
         return this.players;
+    }
+
+    // ゲームの参加者が掛け金をベットするときの処理。CPUはランダムに、人間のplayerは入力を受け取って掛け金を決める。
+    // this.betsの値と各参加者のchipが掛け金分減り、latchが掛け金と同値になる。
+    // chipsが0以下なら順番がスルーされる。
+    public betPhase() : void{
+        for(let i : number = 0; i < this.players.length; ++i){
+            let current : BlackJackPlayer = this.players[i];
+            if(current.getChips() === 0) continue;
+
+            let bet : number = 0;
+            if(current.getType() === "CPU"){
+                bet = Math.random()*(this.players[i].getChips()+1 - 1) + 1;
+            }
+            else {
+                bet = 100 // プレイヤーからの入力を受け取る
+                current.bet(bet);
+            }
+            this.bets[i] = bet;
+            current.bet(bet)
+        }
     }
 }
