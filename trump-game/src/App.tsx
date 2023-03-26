0f468dfcae5b3684d2fcf55451c42dbd61c2bee2
kohei <koheimatsuno99@gmail.com>
2023-03-26 15:26:54 +0900

    removed modules which is not used

--- trump-game/src/App.tsx
+++ trump-game/src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import './App.css';
-import game from './phaser';
 
 function App() {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
