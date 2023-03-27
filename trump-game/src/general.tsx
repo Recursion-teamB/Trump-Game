a3cd251dc0bc259df101a6045bab464a364988af
taisan0816 <guruyo7835@gmail.com>
2023-03-27 21:01:04 +0900

    conflict resolution

--- trump-game/src/general.tsx
+++ trump-game/src/general.tsx
@@ -109,6 +109,9 @@ export class Player{
     public getHand() : Card[]{
         return this.hand;
     }
+    public addHand(card : Card) : void{
+        this.hand.push(card);
+    }
     public getType() : string{
         return this.type;
     }
