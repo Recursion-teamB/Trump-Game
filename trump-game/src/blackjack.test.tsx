1474686f1e26af889353f4e1732d9f60082fd048
taisan0816 <guruyo7835@gmail.com>
2023-03-28 15:24:51 +0900

    conflict resolution

--- trump-game/src/blackjack.test.tsx
+++ trump-game/src/blackjack.test.tsx
@@ -13,8 +13,56 @@ test("betPhase()によってbetsが変更されている", () => {
 
 test("プレイヤーに掛け金がセットされる", () =>{
   table.betPhase();
-  const result = table.getPlayers()[1].getLatch();
+  const result = table.getPlayers()[1].getCost();
   expect(result).toBe(100);
+})
+
+test("hitコマンドのテスト",() => {
+  table.betPhase();
+  let preHandsLen = player.getHand().length
+  if(player.getAction() === ("" || "hit")){
+    player.hit(table.getDeck())
+    expect(player.getHand().length - preHandsLen).toBe(1)
+    if(player.isBust()){
+      expect(player.getAction()).toBe("bust")
+    }else{
+      expect(player.getAction()).toBe("hit")
+    }
+  }
+})
+
+test("standコマンドのテスト",() => {
+  table.betPhase();
+  if(player.getAction() === ("" || "hit")){
+    player.stand()
+    expect(player.getAction()).toBe("stand")
+  }
+})
+
+test("douleコマンドのテスト",()=>{
+  table.betPhase();
+  let preHandsLen = player.getHand().length
+  let preChips = player.getChips()
+  let preCost = player.getCost()
+  if(player.getAction() === ""){
+    player.double(table.getDeck(), 100);
+    expect(player.getHand().length - preHandsLen).toBe(1)
+    if(player.isBust()){
+      expect(player.getAction()).toBe("bust")
+    }else{
+      expect(player.getAction()).toBe("stand")
+    }
+    expect(player.getChips()).toBe(preChips - 100)
+    expect(player.getCost()).toBe(preCost + 100)
+  }
+})
+
+test("surrenderコマンドのテスト", () => {
+  table.betPhase();
+  if(player.getAction()===""){
+    expect(player.getAction()).toBe("surrender")
+    expect(player.getChips()).toBe(39950)
+  }
 })
 
 test('カードの合計を計算する(Aceが1の場合)', () => {
@@ -88,50 +136,3 @@ test("画像の割り当て", () => {
 //   const result = house.calcScore();
 //   expect(result).toBe(20);
 // })
-
-test("hitコマンドのテスト",() => {
-  table.betPhase();
-  let preHandsLen = player.getHand().length
-  if(player.getAction() === "hit"){
-    player.hit(table.getDeck())
-    expect(player.getHand().length - preHandsLen).toBe(1)
-    if(player.isBust()){
-      expect(player.getAction()).toBe("bust")
-    }else{
-      expect(player.getAction()).toBe("hit")
-    }
-  }
-})
-
-test("standコマンドのテスト",() => {
-  table.betPhase();
-  if(player.getAction() === "hit"){
-    player.stand()
-    expect(player.getAction()).toBe("stand")
-  }
-})
-
-test("doubleコマンドのテスト",()=>{
-  table.betPhase();
-  let preHandsLen = player.getHand().length
-  if(player.getAction() === "hit"){
-    player.double(table.getDeck(), 100);
-    expect(player.getHand().length - preHandsLen).toBe(1)
-    if(player.isBust()){
-      expect(player.getAction()).toBe("bust")
-    }else{
-      expect(player.getAction()).toBe("double")
-    }
-    expect(player.getChips()).toBe(39800)
-    expect(player.getCost()).toBe(200)
-  }
-})
-
-test("surrenderコマンドのテスト", () => {
-  table.betPhase();
-  if(player.getAction()==="hit"){
-    expect(player.getAction()).toBe("surrender")
-    expect(player.getChips()).toBe(39950)
-  }
-})
-
