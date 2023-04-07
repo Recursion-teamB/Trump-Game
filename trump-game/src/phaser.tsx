e29ad6f535e3bdac4ffee7a0709f2e77e8c67554
kohei <koheimtri3@gmail.com>
2023-04-07 22:27:01 +0900

    画像を表示できるようにした

--- trump-game/src/phaser.tsx
+++ trump-game/src/phaser.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Phaser from 'phaser';
 import { BlackJackPlayer, BlackJackTable } from './blackjack';
+import { Deck } from './general';
 
 export class MainScene extends Phaser.Scene {
     constructor() {
@@ -11,6 +12,12 @@ export class MainScene extends Phaser.Scene {
       this.load.image('back', 'assets/back.jpg');
       //this.load.image('help', 'assets/buttons/help.png');
       //this.load.image('back_home', 'assets/buttons/back_home.png');
+      const suits = Deck.getSuitList();
+      for (let i = 1; i <= 13; i++) {
+        suits.forEach(suit => {
+          this.load.image(`${suit}${i}`, `card_img/${suit}${i}.png`);
+        });
+      }
     }
   
     create() {
@@ -27,8 +34,8 @@ export class MainScene extends Phaser.Scene {
       // 山札を作成
       const screenWidth = this.cameras.main.width;
       const screenHeight = this.cameras.main.height;
-      const deckWidth = screenWidth * 0.04;
-      const deckHeight = screenHeight * 0.1;
+      const deckWidth = screenWidth * 0.06;
+      const deckHeight = screenHeight * 0.15;
   
       const deck = this.add.image(screenWidth / 2, screenHeight / 2, 'back');
       deck.setDisplaySize(deckWidth, deckHeight);
@@ -36,10 +43,10 @@ export class MainScene extends Phaser.Scene {
       // ディーラーの手札とテキストを作成
       const dealerCards = table.getHouse().getHand();
       dealerCards.forEach((card, index) => {
-        const dealerCard = card.createPhaserImg(this, deck.x - deckWidth * 3 + (index * deckWidth * 1.2), deck.y - deckHeight * 3);
+        const dealerCard = card.createPhaserImg(this, deck.x - 0.5 * deckWidth + (index * deckWidth * 1.2), deck.y - deckHeight * 2);
         dealerCard.setDisplaySize(deckWidth, deckHeight);
       });
-      const dealerText = this.add.text(deck.x - deckWidth / 2, deck.y - deckHeight * 1.0, 'dealer', {
+      const dealerText = this.add.text(deck.x - deckWidth / 2, deck.y - deckHeight * 2.8, 'dealer', {
         fontFamily: 'Arial',
         fontSize: '20px'
       });
@@ -49,14 +56,18 @@ export class MainScene extends Phaser.Scene {
       for (let i = 0; i < numPlayers; i++) {
         const playerCard = table.getPlayers()[i].getHand()
         playerCard.forEach((card, index) => {
-          card.createPhaserImg(this, deck.x + (i - 1) * deckWidth * 3 + (index * deckWidth * 1.2), deck.y + deckHeight * 3);
+          card.createPhaserImg(this, deck.x + (i - 1.2) * deckWidth * 3 + (index * deckWidth * 1.3), deck.y + deckHeight * 2);
           card.getPhaserImage().setDisplaySize(deckWidth, deckHeight);
         });
-        //const playerText = this.add.text(deck.x - (i - 1) * deckWidth * 3, deck.y - deckHeight * 1.0, 'player');
+        const playerText = this.add.text(deck.x + (i - 1.2) * deckWidth * 3, deck.y + deckHeight * 2.8, 'player', {
+          fontFamily: 'Arial',
+          fontSize: '20px'
+        });
       }
     }
 }
-  
+
+
 
 export const GameContainer: React.FC = () => {
     useEffect(() => {
