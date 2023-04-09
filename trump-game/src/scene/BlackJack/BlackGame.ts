5cbd27b0a67626d67a027eaa97f7fdcd46e1c364
kohei <koheimtri3@gmail.com>
2023-04-09 15:58:54 +0900

    一旦不要なメソッドを削除

--- trump-game/src/scene/BlackJack/BlackGame.ts
+++ trump-game/src/scene/BlackJack/BlackGame.ts
@@ -1,56 +1,91 @@
 import { BlackJackPlayer, BlackJackTable } from '../../model/BlackJack/blackjack';
+import Phaser from 'phaser';
+import { Deck } from '../../model/General/general';
+
+
 
 export default class BlackGameScene extends Phaser.Scene {
+    private playerChipsTexts: Phaser.GameObjects.Text[] = [];
+    private playerScoresTexts: Phaser.GameObjects.Text[] = [];
     constructor() {
-        super({key: "BlackGameScene"})
+      super({ key: 'BlackGameScene' });
     }
-
+  
     preload() {
-        this.load.image('back', 'assets/back.jpg');
-        //this.load.image('help', 'assets/buttons/help.png');
-        //this.load.image('back_home', 'assets/buttons/back_home.png');
-    }
-    
-    create() {
-        let player : BlackJackPlayer = new BlackJackPlayer("Player", "Player");
-        let table : BlackJackTable = new BlackJackTable(player);
-        table.distributeCards();
-        // 背景を緑色に設定
-        this.cameras.main.setBackgroundColor(0x008800);
-    
-        // ヘルプボタンとバックホームボタンを作成
-        const helpButton = this.add.image(50, 50, 'help').setInteractive();
-        const backHomeButton = this.add.image(150, 50, 'back_home').setInteractive();
-    
-        // 山札を作成
-        const screenWidth = this.cameras.main.width;
-        const screenHeight = this.cameras.main.height;
-        const deckWidth = screenWidth * 0.04;
-        const deckHeight = screenHeight * 0.1;
-    
-        const deck = this.add.image(screenWidth / 2, screenHeight / 2, 'back');
-        deck.setDisplaySize(deckWidth, deckHeight);
-    
-        // ディーラーの手札とテキストを作成
-        const dealerCards = table.getHouse().getHand();
-        dealerCards.forEach((card, index) => {
-          const dealerCard = card.createPhaserImg(this, deck.x - deckWidth * 3 + (index * deckWidth * 1.2), deck.y - deckHeight * 3);
-          dealerCard.setDisplaySize(deckWidth, deckHeight);
+      this.load.image('back', 'assets/back.jpg');
+      //this.load.image('help', 'assets/buttons/help.png');
+      //this.load.image('back_home', 'assets/buttons/back_home.png');
+      const suits = Deck.getSuitList();
+      for (let i = 1; i <= 13; i++) {
+        suits.forEach(suit => {
+          this.load.image(`${suit}${i}`, `assets/card_img/${suit}${i}.png`);
         });
-        const dealerText = this.add.text(deck.x - deckWidth / 2, deck.y - deckHeight * 1.0, 'dealer', {
-            fontFamily: 'Arial',
-            fontSize: '20px'
-        });
-    
-        // プレイヤーの手札とテキストを作成
-        const numPlayers = table.getPlayers().length;
-        for (let i = 0; i < numPlayers; i++) {
-            const playerCard = table.getPlayers()[i].getHand()
-            playerCard.forEach((card, index) => {
-                card.createPhaserImg(this, deck.x + (i - 1) * deckWidth * 3 + (index * deckWidth * 1.2), deck.y + deckHeight * 3);
-                card.getPhaserImage().setDisplaySize(deckWidth, deckHeight);
-          });
-          //const playerText = this.add.text(deck.x - (i - 1) * deckWidth * 3, deck.y - deckHeight * 1.0, 'player');
-        }
       }
+    }
+  
+    create() {
+      let player: BlackJackPlayer = new BlackJackPlayer("Player", "Player");
+      let table: BlackJackTable = new BlackJackTable(player);
+      table.distributeCards();
+      const screenWidth = this.cameras.main.width;
+      const screenHeight = this.cameras.main.height;
+      this.cameras.main.setBackgroundColor(0x008800);
+      this.scene.launch('BlackGameSceneController');
+      this.createHelpAndHomeButtons();
+      this.createDeck(screenWidth, screenHeight);
+      this.createDealerSection(table, screenWidth / 2, screenHeight / 2);
+      this.createPlayerSection(table, screenWidth / 2, screenHeight / 2);
+    }
+    createHelpAndHomeButtons() {
+      const helpButton = this.add.image(50, 50, 'help').setInteractive();
+      const backHomeButton = this.add.image(150, 50, 'back_home').setInteractive();
+    }
+    createDeck(screenWidth: number, screenHeight: number) {
+      const deckWidth = screenWidth * 0.06;
+      const deckHeight = screenHeight * 0.15;
+      const deck = this.add.image(screenWidth / 2, screenHeight / 2 , 'back');
+      deck.setDisplaySize(deckWidth, deckHeight);
+    }
+    createDealerSection(table: BlackJackTable, screenWidth: number, screenHeight: number) {
+      const dealerCards = table.getHouse().getHand();
+      dealerCards.forEach((card, index) => {
+        const dealerCard = card.createPhaserImg(this, screenWidth - 0.5 * screenWidth * 0.06 + (index * screenWidth * 0.06 * 0.8), screenHeight - screenHeight * 0.3 * 1.5);
+        dealerCard.setDisplaySize(screenWidth * 0.12, screenHeight * 0.3);
+      });
+      const dealerText = this.add.text(screenWidth - screenWidth * 0.06, screenHeight - screenHeight * 0.3 * 2.5, 'dealer', {
+        fontFamily: 'Arial',
+        fontSize: '20px'
+      });
+    }
+    createPlayerSection(table: BlackJackTable, screenWidth: number, screenHeight: number) {
+      const numPlayers = table.getPlayers().length;
+      const spaceBetweenPlayers = screenWidth / (numPlayers + 1);
+      for (let i = 0; i < numPlayers; i++) {
+        const player = table.getPlayers()[i];
+        const playerCards = player.getHand();
+        const playerX = (i + 1) * spaceBetweenPlayers;
+    
+        playerCards.forEach((card, index) => {
+          let playerCard = card.createPhaserImg(this, playerX + (index * screenWidth * 0.06 * 0.8), screenHeight + screenHeight * 0.3 * 1.5);
+          playerCard.setDisplaySize(screenWidth * 0.12, screenHeight * 0.3);
+        });
+    
+        const playerText = this.add.text(playerX, screenHeight + screenHeight * 0.3 * 2.2, `player : ${player.getType()}`, {
+          fontFamily: 'Arial',
+          fontSize: '20px'
+        });
+        const playerChip = this.add.text(playerX, screenHeight + screenHeight * 0.3 * 2.5, `$${player.getChips()}`, {
+          fontFamily: 'Arial',
+          fontSize: '20px'
+        });
+
+        const playerScore = this.add.text(playerX, screenHeight + screenHeight * 0.3 * 2.8, `Score : ${player.calcScore()}`, {
+          fontFamily: 'Arial',
+          fontSize: '20px'
+        });
+        this.playerChipsTexts[i] = playerChip;
+        this.playerScoresTexts[i] = playerScore;
+      }
+      
+    }
 }
