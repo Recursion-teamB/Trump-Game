183378e3546f1e36cc8346eb5ab74488ea7a5e32
taisan0816 <guruyo7835@gmail.com>
2023-04-13 21:20:05 +0900

    conflict resolution

--- trump-game/src/model/BlackJack/blackjack.tsx
+++ trump-game/src/model/BlackJack/blackjack.tsx
@@ -133,6 +133,23 @@ export class BlackJackPlayer extends Player{
         this.setAction("surrender")
         console.log('surrender');
     }
+        //roundResultにはwin, draw, loseが入る. 手札が2枚で21かを判定する.
+    //judgePerRound()メソッドで呼び出す.
+    //賞金をchipsに追加し, costをリセットする.
+    public winPrize(roundResult : string){
+        if(roundResult === "win"){
+            if(this.calcScore() == 21 && this.getHand().length == 2){
+                this.addChips(this.getCost() * 2.5)
+            }else{
+                this.addChips(this.getCost() * 2)
+            }
+            
+        }else if(roundResult === "draw"){
+            this.addChips(this.getCost())
+        }
+        this.setCost(0)
+    }
+
 }
 
 export class BlackJackTable {
@@ -265,8 +282,29 @@ export class BlackJackTable {
         this.actionPhase(scene);
     }
 
-    public judgeWinOrLose() : string[]{
-        return ["win","win","win"]
+    
+    public judgePerRound() : void{
+        const dealerScore = this.house.calcScore()
+        let result = ""
+        const judge = (num1 : number, num2 : number) => {
+            if(num1 > num2){
+                return "win"
+            }else if(num1 === num2){
+                return "draw"
+            }else{
+                return "lose"
+            }
+        }
+        for(const player of this.players){
+            if(player.getAction() === "surrender" || player.getAction() === "bust"){
+                result = "lose"
+            }else if(this.house.getAction() === "bust"){
+                result = "win"
+            }else{
+                result = judge(player.calcScore(),dealerScore)
+            }
+            player.winPrize(result)
+        }
     }
 
     public getPhase(){
