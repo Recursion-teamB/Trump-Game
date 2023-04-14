5ea044cd55112395496037de4337f1a7dfcc3db4
taisan0816 <guruyo7835@gmail.com>
2023-04-14 17:47:09 +0900

    #50 bl ide crelevelScene's cotents of create

--- trump-game/src/scene/BlackJack/BlackLevel.ts
+++ trump-game/src/scene/BlackJack/BlackLevel.ts
@@ -42,84 +42,22 @@ export default class BlackLevelScene extends Phaser.Scene{
         // レベル選択用のテキスト
         this.createLevelButtons() 
 
-        // 敵の数選択用のテキスト
-        const selectedEnemyCountText = this.add.text(this.widthPosition(0.2), this.heightPosition(0.5), '', { fontSize: '20px', color: '#fff' });
-        selectedEnemyCountText.setOrigin(0.5, 0.5);
-        updateSelectedEnemyCount(3)
+        // 敵の数のテキストとボタン
+        this.createEnemyButtons()
 
-        // 敵の数選択用のボタン
-        const enemyButtons = Array.from({ length: 6 }, (_, index) => {
-            const button = this.add.text(this.widthPosition(0.1) + (index * this.widthPosition(0.05)), this.heightPosition(0.6), (index + 1).toString(), { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
-            button.setOrigin(0.5, 0.5);
-            button.setInteractive();
-
-            button.on('pointerdown', () => {
-                console.log(`Selected enemy count: ${index + 1}`);
-                updateSelectedEnemyCount(index + 1); // 選択した敵の数を更新
-                BlackLevelScene.setCount(index + 1)
-            });
-
-            return button;
-        });
-
-
-        const ruleFrameX : number = this.widthPosition(0.45)
-        const ruleFrameY = this.heightPosition(0.22)
-        const ruleWidth = this.widthPosition(0.47)
-        const ruleHight = this.heightPosition(0.55)
-        const graphics = this.add.graphics();
-        graphics.lineStyle(4, 0xffffff, 1);
-        graphics.strokeRect(ruleFrameX, ruleFrameY, ruleWidth, ruleHight);
-
-        const blackjackRuleText = `
-            ブラックジャックでは、カードの合計値が21と
-            なるような手札を持つこと、またはその制限を
-            超えない範囲でできるだけそれに近い手札を
-            持つことが目標とされます。 ディーラーと対戦し、
-            21に近づけるためにもう1枚カードをもらう
-            (ヒット）か、持っているカードをそのままにする
-            （スタンド）かのどちらかを21を超えない範囲で
-            最も高い手札の価値を持つプレイヤーが
-            ゲームに勝利します。
-        `
-        const rulesText = this.add.text(ruleFrameX + 10, ruleFrameY + 10, blackjackRuleText, { fontSize: '16px', color: '#fff', wordWrap: { width: ruleWidth - 20 } });
-
+        //ルール追加
+        this.createRulesText() 
 
         //3つのボタン追加
-        const buttonSpacing = this.widthPosition(0.125); // ボタン間のスペース
-        const buttonY = this.centerY + 250; // ボタンのy座標
-
-        const startButton = this.add.text(this.centerX - buttonSpacing, buttonY, 'START', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
-        startButton.setOrigin(1, 0.5);
-        startButton.setInteractive();
-        startButton.on('pointerdown', () => {
-            this.scene.start('BlackGameScene');
-            this.scene.stop('BlackLevelScene');
-        })
-        const quitButton = this.add.text(this.centerX, buttonY, 'QUIT', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
-        quitButton.setOrigin(0.5, 0.5);
-        quitButton.setInteractive();
-        quitButton.on('pointerdown', () => {
-            this.scene.start('LobbyScene');
-            this.scene.stop('BlackLevelScene');
-        })
-
-        const tutorialButton = this.add.text(this.centerX + buttonSpacing, buttonY, 'TUTORIAL', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
-        tutorialButton.setOrigin(0, 0.5);
-        tutorialButton.setInteractive();
-        // tutorialButtonのイベントハンドラをここに追加...
-
-        
-
-        function updateSelectedEnemyCount(count : number) : void {
-            selectedEnemyCountText.setText(`敵の数: ${count}`);
-        }
+        this.createControlButtons() 
     }
+    //タイトルの追加
     createTitleText() {
         const titleText = this.add.text(this.centerX, this.heightPosition(0.1), 'ブラックジャック', { fontSize: '32px', color: '#fff' });
         titleText.setOrigin(0.5, 0.5);
     }
 
+    //難易度選択
     createLevelButtons() {
         const selectedLevelText = this.add.text(this.widthPosition(0.2), this.heightPosition(0.3), '', { fontSize: '20px', color: '#fff' });
         selectedLevelText.setOrigin(0.5, 0.5);
@@ -138,12 +76,92 @@ export default class BlackLevelScene extends Phaser.Scene{
             return button;
         });
     }
+
+    //難易度選択時に更新するためのメソッド
     updateSelectedLevel(level : string, selectedLevelText: Phaser.GameObjects.Text) : void{
         BlackLevelScene.setLevel(level)
         selectedLevelText.setText(`レベル: ${BlackLevelScene.getLevel().toUpperCase()}`);
     }
 
-    //引数にnumをとることで, 割合をかけて得ることができる.
+    //敵の数を選択するボタン
+    createEnemyButtons() {
+        const selectedEnemyCountText = this.add.text(this.widthPosition(0.2), this.heightPosition(0.5), '', { fontSize: '20px', color: '#fff' });
+        selectedEnemyCountText.setOrigin(0.5, 0.5);
+        this.updateSelectedEnemyCount(3, selectedEnemyCountText)
+
+        const enemyButtons = Array.from({ length: 6 }, (_, index) => {
+            const button = this.add.text(this.widthPosition(0.1) + (index * this.widthPosition(0.05)), this.heightPosition(0.6), (index + 1).toString(), { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
+            button.setOrigin(0.5, 0.5);
+            button.setInteractive();
+
+            button.on('pointerdown', () => {
+                console.log(`Selected enemy count: ${index + 1}`);
+                this.updateSelectedEnemyCount(index + 1, selectedEnemyCountText); // 選択した敵の数を更新
+                BlackLevelScene.setCount(index + 1)
+            });
+
+            return button;
+        });
+    }
+
+    //敵の数選択時に更新するためのボタン
+    updateSelectedEnemyCount(count : number, selectedEnemyCountText: Phaser.GameObjects.Text) : void {
+        BlackLevelScene.setCount(count)
+        selectedEnemyCountText.setText(`敵の数: ${count}`);
+    }
+
+    //ルールを追加するためのメソッド
+    createRulesText() {
+        const ruleFrameX = this.widthPosition(0.45)
+        const ruleFrameY = this.heightPosition(0.22)
+        const ruleWidth = this.widthPosition(0.47)
+        const ruleHight = this.heightPosition(0.55)
+        const graphics = this.add.graphics();
+        graphics.lineStyle(4, 0xffffff, 1);
+        graphics.strokeRect(ruleFrameX, ruleFrameY, ruleWidth, ruleHight);
+
+        const blackjackRuleText = `
+        ブラックジャックでは、カードの合計値が21と
+        なるような手札を持つこと、またはその制限を
+        超えない範囲でできるだけそれに近い手札を
+        持つことが目標とされます。 ディーラーと対戦し、
+        21に近づけるためにもう1枚カードをもらう
+        (ヒット）か、持っているカードをそのままにする
+        （スタンド）かのどちらかを21を超えない範囲で
+        最も高い手札の価値を持つプレイヤーが
+        ゲームに勝利します。
+        `
+        const rulesText = this.add.text(ruleFrameX + 10, ruleFrameY + 10, blackjackRuleText, { fontSize: '16px', color: '#fff', wordWrap: { width: ruleWidth - 20 } });
+    }
+
+    //start, quit, tutorialボタン追加
+    createControlButtons() {
+        const buttonSpacing = this.widthPosition(0.125);
+        const buttonY = this.centerY + 250;
+
+        const startButton = this.add.text(this.centerX - buttonSpacing, buttonY, 'START', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
+        startButton.setOrigin(1, 0.5);
+        startButton.setInteractive();
+        startButton.on('pointerdown', () => {
+            this.scene.start('BlackGameScene');
+            this.scene.stop('BlackLevelScene');
+        })
+
+        const quitButton = this.add.text(this.centerX, buttonY, 'QUIT', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
+        quitButton.setOrigin(0.5, 0.5);
+        quitButton.setInteractive();
+        quitButton.on('pointerdown', () => {
+            this.scene.start('LobbyScene');
+            this.scene.stop('BlackLevelScene');
+        })
+
+        const tutorialButton = this.add.text(this.centerX + buttonSpacing, buttonY, 'TUTORIAL', { fontSize: '20px', color: '#fff', backgroundColor: '#1a1a1a'});
+        tutorialButton.setOrigin(0, 0.5);
+        tutorialButton.setInteractive();
+        // tutorialButtonのイベントハンドラをここに追加...
+    }
+
+    //引数にnumをとることで, 画面の幅に対する割合をかけてその数字を利用することができる.
     widthPosition(num : number) : number{
         if(num <= 1 && num >= 0){
             return this.width * num
